d64278a21e0c379913777cbb5a402202e682e1d8
김근호 <kimkgh8@gmail.com>
2023-03-08 08:07:43 +0900

    ✨ Feat: React App에서의 prop-types 사용
    CSS 모듈화를 통한 스타일 지정
    똑같은 ClassName임에도 불구하고 상관없음.

--- react-for-movie-app/src/App.js
+++ react-for-movie-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
 	<div>
-		<h1>Welcome back!</h1>
+		<h1 className={styles.title}>Welcome back!</h1>
+		<Button text={"Continue"} />
 	</div>
   );
 }
