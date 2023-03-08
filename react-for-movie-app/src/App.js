ddad2c55f11b744f4bc5a7a0557a237edb6fd819
김근호 <kimkgh8@gmail.com>
2023-03-09 07:21:07 +0900

    ✨ Feat: useEffect의 활용에 대해 학습
    코드의 실행 시점을 관리할 수 있는 선택권을 얻는 방어막 같은 존재, 디펜던시가 없을 경우 최초 1회 실행, 있을 경우 해당 값이 변할 경우 실행한다. 이 때 디펜던시는 여러개 입력이 가능하다.
    *부모 컴포넌트에서 리렌더링이 일어날 경우 모든 자식들이 리렌더링이 된다.*

--- react-for-movie-app/src/App.js
+++ react-for-movie-app/src/App.js
@@ -1,11 +1,31 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+	setKeword(event.target.value);
+  }
+  useEffect( () => {
+	console.log("I run only once.")
+	}, []);
+  useEffect(() => {
+	console.log("I run when 'keyword' changs.");
+  }, [keyword]);
+  useEffect(() => {
+	console.log("I run when 'counter' changs.");
+  }, [counter]);
+  useEffect(() => {
+	console.log("I run when 'counter & keyword' changs.");
+  }, [keyword, counter]);
+
   return (
 	<div>
-		<h1 className={styles.title}>Welcome back!</h1>
-		<Button text={"Continue"} />
+		<input type="text" placeholder="Search here..." onChange={onChange} value={keyword}/>
+		<h1 className={styles.title}>{counter}</h1>
+		<button onClick={onClick}>click me</button>
 	</div>
   );
 }
