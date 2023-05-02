b1f038ab0ebb1959bbe8c769969ba820cfe50822
김근호 <kimkgh8@gmail.com>
2023-05-02 23:17:29 +0900

    ✨ Feat : Movie App 만들기 중간 커밋
    1. 컴포넌트화
    2. 화면 이동

--- react-for-movie-app/src/App.js
+++ react-for-movie-app/src/App.js
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
-
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from "react-router-dom";
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
 function App() {
-	const [loading, setLoading] = useState(true);
-	const [movies, setMovies] = useState([]);
-	const getMovies = async() => {
-		const json = await ((await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json())
-		setMovies(json.data.movies);
-		setLoading(false);
-	}
-	useEffect(() => {
-		getMovies();
-	}, []);
-	console.log(movies)
-  return (
-	<div>
-		{loading ? <h3>Loading...</h3> : null}
-	</div>
-  );
+	return <Router>
+		<Switch>
+			<Route path="/movie">
+				<Detail />
+			</Route>
+			<Route path="/">
+				<Home />
+			</Route>
+		</Switch>
+	</Router>;
 }
 
 export default App;
